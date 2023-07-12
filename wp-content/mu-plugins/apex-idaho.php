1fbe4af24e6c06047c4b4f8c6627ba766bc83cb0
Nic Ford <nicdford@gmail.com>
2023-07-12 12:26:42 -0600

    Trying to figure out mu-plugins

--- wp-content/mu-plugins/apex-idaho.php
+++ wp-content/mu-plugins/apex-idaho.php
@@ -6,13 +6,15 @@
  * @param string $visited_url The visited URL.
  * @return bool
  */
-function my_forcelogin_bypass( $bypass, $visited_url ) {
+// function my_forcelogin_bypass( $bypass, $visited_url ) {
 
-  // Allow 'My Page' to be publicly accessible
-  if ( is_page('home') ) {
-    $bypass = true;
-  }
+//   // Allow 'My Page' to be publicly accessible
+//   if ( is_page('home') ) {
+//     $bypass = true;
+//   }
 
-  return $bypass;
-}
-add_filter( 'v_forcelogin_bypass', 'my_forcelogin_bypass', 10, 2 );
+//   return $bypass;
+// }
+// add_filter( 'v_forcelogin_bypass', 'my_forcelogin_bypass', 10, 2 );
+
+echo "Hello World!";

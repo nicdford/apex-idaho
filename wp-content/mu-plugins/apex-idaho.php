eaf15bb4204f8e2fbee9aecaea123d019703eb70
Nic Ford <nicdford@gmail.com>
2023-07-12 12:31:11 -0600

    Add plugin header

--- wp-content/mu-plugins/apex-idaho.php
+++ wp-content/mu-plugins/apex-idaho.php
@@ -1,20 +1,24 @@
 <?php 
 /**
+ * Plugin Name: APEX Idaho Customizations
+ * Description: A home for PHP customizations that can be made to the APEX Idaho site. 
+ * Author:      Nic D. Ford
+ */
+
+/**
  * Bypass Force Login to allow for exceptions.
  *
  * @param bool $bypass Whether to disable Force Login. Default false.
  * @param string $visited_url The visited URL.
  * @return bool
  */
-// function my_forcelogin_bypass( $bypass, $visited_url ) {
+function my_forcelogin_bypass( $bypass, $visited_url ) {
 
-//   // Allow 'My Page' to be publicly accessible
-//   if ( is_page('home') ) {
-//     $bypass = true;
-//   }
+  // Allow 'My Page' to be publicly accessible
+  if ( is_page('home') ) {
+    $bypass = true;
+  }
 
-//   return $bypass;
-// }
-// add_filter( 'v_forcelogin_bypass', 'my_forcelogin_bypass', 10, 2 );
-
-echo "Hello World!";
+  return $bypass;
+}
+add_filter( 'v_forcelogin_bypass', 'my_forcelogin_bypass', 10, 2 );

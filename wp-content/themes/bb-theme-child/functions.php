cd4e2d89d3de4f85173c1315b450c82ef89f4fd4
Nic Ford <nicdford@gmail.com>
2024-04-04 00:35:04 -0600

    Add logic to enforce coupon codes on grass valley

--- wp-content/themes/bb-theme-child/functions.php
+++ wp-content/themes/bb-theme-child/functions.php
@@ -1,25 +1,59 @@
 <?php
 
 // Defines
-define( 'FL_CHILD_THEME_DIR', get_stylesheet_directory() );
-define( 'FL_CHILD_THEME_URL', get_stylesheet_directory_uri() );
+define('FL_CHILD_THEME_DIR', get_stylesheet_directory());
+define('FL_CHILD_THEME_URL', get_stylesheet_directory_uri());
 
 // Classes
 require_once 'classes/class-fl-child-theme.php';
 
 // Actions
-add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
+add_action('wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000);
 
 /**
  * Auto Complete all WooCommerce orders.
  */
-add_action( 'woocommerce_thankyou', 'custom_woocommerce_auto_complete_order' );
+add_action('woocommerce_thankyou', 'custom_woocommerce_auto_complete_order');
 
-function custom_woocommerce_auto_complete_order( $order_id ) { 
-    if ( !$order_id ) {
-        return;
+function custom_woocommerce_auto_complete_order($order_id)
+{
+	if (!$order_id) {
+		return;
+	}
+
+	$order = wc_get_order($order_id);
+	$order->update_status('completed');
+}
+
+/**
+ * Require coupon for checkout on grass valley driver entry tickets
+ */
+add_action('woocommerce_check_cart_items', 'mandatory_coupon_for_grass_valley_fos');
+function mandatory_coupon_for_grass_valley_fos()
+{
+    $targeted_ids = array(807); // The targeted product ids (in this array)
+    $coupon_codes = ['STUZSBWX', 'URCHQDGD', 'T338RSPY', '86JZ63RR', 'AJMSDUM2']; // The required coupon codes
+
+    $applied_coupons = WC()->cart->get_applied_coupons();
+
+    // Check if any of the coupon codes are applied
+    $valid_coupon_applied = false;
+    foreach ($coupon_codes as $code) {
+        if (in_array(strtolower($code), $applied_coupons)) {
+            $valid_coupon_applied = true;
+            break;
+        }
     }
- 
-    $order = wc_get_order( $order_id );
-    $order->update_status( 'completed' );
+
+    // Loop through cart items
+    foreach (WC()->cart->get_cart() as $cart_item) {
+        // Check cart item for defined product Ids and applied coupon
+        if (in_array($cart_item['product_id'], $targeted_ids) && !$valid_coupon_applied) {
+            wc_clear_notices(); // Clear all other notices
+
+            // Avoid checkout displaying an error notice
+            wc_add_notice(sprintf('The product "%s" requires a coupon for checkout.', $cart_item['data']->get_name()), 'error');
+            break; // stop the loop
+        }
+    }
 }

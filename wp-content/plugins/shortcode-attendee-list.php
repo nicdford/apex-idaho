414e7f1b5d99c2631e2d953c29f9c60e801f4b02
Nic Ford <nicdford@gmail.com>
2024-09-08 19:54:19 -0600

    Update shortcode-attendee-list.php

--- wp-content/plugins/shortcode-attendee-list.php
+++ wp-content/plugins/shortcode-attendee-list.php
@@ -1,41 +1,67 @@
 <?php
 /*
 Plugin Name: Attendee List
-Description: Display the attendees for a given ticket ID>
+Description: Display the attendees for a given ticket ID.
 Version: 1.0.0
 Author: Nic D. Ford
 Author URI: https://nicdford.com
  */
 
+// Prevent direct access to the file
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Add shortcode
+add_shortcode('attendee_list', 'attendee_list_shortcode');
+
 function attendee_list_shortcode($atts)
 {
-    $atts = shortcode_atts(array(
-        'ticket_id' => '',
-    ), $atts);
-
-    // Query the attendees based on the ticket ID
-    $args = array(
-        'post_type' => 'attendee',
-        'meta_key' => 'ticket_id',
-        'meta_value' => $atts['ticket_id'],
+    // Extract attributes
+    $atts = shortcode_atts(
+        array(
+            'ticket_id' => '',
+        ),
+        $atts,
+        'attendee_list'
     );
-    $attendees_query = new WP_Query($args);
 
-    // Check if there are any attendees
-    if ($attendees_query->have_posts()) {
-        $output = '<ul>';
-        while ($attendees_query->have_posts()) {
-            $attendees_query->the_post();
-            $output .= '<li>' . get_the_title() . '</li>';
-        }
-        $output .= '</ul>';
-    } else {
-        $output = 'No attendees found.';
+    // Check if event_id is provided
+    if (empty($atts['event_id'])) {
+        return 'Please provide an event ID.';
     }
 
-    // Reset the query
-    wp_reset_postdata();
+    // Check if The Events Calendar and Event Tickets are active
+    if (!class_exists('Tribe__Events__Main') || !class_exists('Tribe__Tickets__Main')) {
+        return 'The Events Calendar and Event Tickets plugins are required.';
+    }
 
-    return $output;
+    // Get attendees
+    $attendees = tribe_tickets_get_attendees($atts['event_id']);
+
+    if (empty($attendees)) {
+        return 'No attendees found for this event.';
+    }
+
+    // Start output buffering
+    ob_start();
+
+    // Display attendees
+    echo '<table class="attendee-list">';
+    echo '<thead><tr><th>Name</th><th>Email</th><th>Ticket</th></tr></thead>';
+    echo '<tbody>';
+
+    foreach ($attendees as $attendee) {
+        echo '<tr>';
+        echo '<td>' . esc_html($attendee['holder_name']) . '</td>';
+        echo '<td>' . esc_html($attendee['holder_email']) . '</td>';
+        echo '<td>' . esc_html($attendee['ticket']) . '</td>';
+        echo '</tr>';
+    }
+
+    echo '</tbody>';
+    echo '</table>';
+
+    // Return the buffered content
+    return ob_get_clean();
 }
-add_shortcode('attendee_list', 'attendee_list_shortcode');
